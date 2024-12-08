002cd78e82d2bf3ca91e7c93e6d07fc7e16e553a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-08 23:43:33 +0100

    remove

--- app/site/links.ts
+++ app/site/links.ts
@@ -2,7 +2,7 @@ export const  links = [
     {link: '/site', label: "Acceuil"},
     {link: '/site/solution', label: "A Propos"},
     {link: '/site/service', label: "Nos services"},
-    {link: '/site/mes-travaux', label: "Mes travaux"},
+    // {link: '/site/mes-travaux', label: "Mes travaux"},
     {link: '/site/contact', label: "Contact"},
 ]
 
