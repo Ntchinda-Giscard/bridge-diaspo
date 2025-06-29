5c72aa9909b20ba1b13b8c390a5d8c1c4451cbf1
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-06-29 16:18:40 +0100

    Add Apollo Client integration and update dependencies

--- app/site/providers/provider.tsx
+++ app/site/providers/provider.tsx
@@ -8,17 +8,37 @@ import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import AuthProvider from './AuthProvider';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
+
 
 export const metadata = {
   title: 'My Mantine app',
   description: 'I have followed setup instructions carefully',
 };
 
+
+
 export default function Provider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const adminSecret = "rGvtJPPzqVUtdlnOA47eMp3DhiiNqqAF";
+    const httpLink = new HttpLink({
+        uri: "https://cheerful-crane-98.hasura.app/v1/graphql", // Replace with your Hasura GraphQL URL
+        headers: {
+            "x-hasura-admin-secret": adminSecret, // Replace with your admin secret
+        },
+    });
+
+// Create the Apollo Client instance
+    const client = new ApolloClient({
+        link: httpLink,
+        cache: new InMemoryCache(),
+    });
+
+
   return (
     <html lang="en">
       <head>
@@ -26,13 +46,15 @@ export default function Provider({
       </head>
       <body>
         <MantineProvider>
-          <AuthProvider>
-          <TawkMessengerReact
-            propertyId="67079eb902d78d1a30ef6cbb"
-            widgetId="1i9qs823g"
-          />
-          {children}
-          </AuthProvider>
+          <ApolloProvider client={client}>
+            <AuthProvider>
+            <TawkMessengerReact
+              propertyId="67079eb902d78d1a30ef6cbb"
+              widgetId="1i9qs823g"
+            />
+            {children}
+            </AuthProvider>
+          </ApolloProvider>
         </MantineProvider>
       </body>
     </html>
