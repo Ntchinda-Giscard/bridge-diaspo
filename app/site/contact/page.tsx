16dbd5050863135ad322705bc4ebf87906d740e8
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-07-02 10:17:15 +0100

    Replace MyGoogleMap component with an iframe for Google Maps integration

--- app/site/contact/page.tsx
+++ app/site/contact/page.tsx
@@ -153,7 +153,16 @@ function Contact() {
 
             </div>
         </div>
-        <MyGoogleMap />
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.019389118592!2d-122.41941508468197!3d37.77492927975978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858064bf2c9a2b%3A0x27fcedea3a83cb6f!2sSan%20Francisco!5e0!3m2!1sen!2sus!4v1614657763473!5m2!1sen!2sus"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade">
+        </iframe>
+
     </> );
 }
 
